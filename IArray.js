8b6ba009441ad8b46753e4e174850aaab88cf360
Arcsin <Arcsin>
2017-07-22 00:01:05 +0300

    FreeProxyList instead of HideMyAss

--- IArray.js
+++ IArray.js
@@ -9,5 +9,22 @@ exports.IArray = {
         return a.filter(function (n) {
             return IString.isNumeric(n);
         });
+    },
+    /**
+     * @param {Array} keys
+     * @param {Array} values
+     * @returns {Object}
+     * @throws {Error}
+     */
+    fillKeys: function (keys, values) {
+        if (keys.length === values.length) {
+            for (var i = 0, fillKeysObject = {}; i < keys.length; ++i) {
+                fillKeysObject[keys[i]] = values[i];
+            }
+
+            return fillKeysObject;
+        }
+
+        throw new Error("Error: 'keys' length must match 'values' length.");
     }
 };
