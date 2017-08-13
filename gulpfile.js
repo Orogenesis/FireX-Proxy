663a71f88181fa52de1eb079f7b40479d3bfab98
Arcsin <Arcsin>
2017-08-13 11:10:53 +0300

    .jpmignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-const gulp = require('gulp');
+const gulp   = require('gulp');
 const coffee = require('gulp-coffee');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
 
 gulp.task('coffee', function() {
-    gulp.src([
-        './data/locale/*.coffee',
-        './data/coffee/**/*.coffee'
-    ]).pipe(coffee({
-        bare: true
-    })).pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./data/build'));
+    gulp
+        .src([
+            './data/locale/*.coffee',
+            './data/coffee/**/*.coffee'
+        ])
+        .pipe(coffee({
+            bare: true
+        }))
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./data/build'));
 });
 
 gulp.task('default', [
