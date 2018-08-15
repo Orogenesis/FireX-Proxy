211016aa4765b9349f23d6112ec303bb8c1da21e
Arcsin <evansiveyt@gmail.com>
2018-08-16 02:16:02 +0300

    Firefox pac

--- addon/pac/firefox.js
+++ addon/pac/firefox.js
@@ -3,6 +3,87 @@ let blacklist          = {};
 let isBlacklistEnabled = false;
 
 /**
+ * @param {string} str
+ * @param {string} shexp
+ * @returns {boolean}
+ */
+function shExpMatch(str, shexp) {
+    if (typeof str !== "string" || typeof shexp !== "string") {
+        return false;
+    }
+
+    if (shexp === "*") {
+        return true;
+    }
+
+    if (str === "" && shexp === "") {
+        return true;
+    }
+
+    str = str.toLowerCase();
+    shexp = shexp.toLowerCase();
+
+    let len = str.length;
+    let pieces = shexp.split('*');
+    let start = 0;
+
+    for (let i = 0; i < pieces.length; i++) {
+        if (pieces[i] === "") {
+            continue;
+        }
+
+        if (start > len) {
+            return false;
+        }
+
+        start = str.indexOf(pieces[i]);
+
+        if (start === -1) {
+            return false;
+        }
+
+        start += pieces[i].length;
+
+        str = str.substring(start, len);
+        len = str.length;
+    }
+
+    i--;
+
+    return pieces[i] === "" || str === "";
+}
+
+/**
+ * @param {string} host
+ * @param {string} pattern
+ * @param {string} mask
+ * @returns {boolean}
+ */
+function isInNet(host, pattern, mask) {
+    let b = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/.exec(host);
+
+    if (b[1] > 255 || b[2] > 255 || b[3] > 255 || b[4] > 255) {
+        return false;
+    }
+
+    let p = pattern.split('.');
+    let m = mask.split('.');
+    let h = host.split('.');
+
+    if ((p.length === m.length) && (m.length === a.length)) {
+        for (let i = 0; i < p.length; i++) {
+            if ((p[i] & m[i]) !== (m[i] & h[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+/**
  * @param {string} url
  * @param {string} host
  *
